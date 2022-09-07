1d241aeafb63bc985bec617039bd269251e9273d
Alexander Beattie <alexander.beattie@quarkworks.co>
2022-09-07 15:27:02 +0300

    Unify protobuf naming and update to match osc streaming order

--- build.py
+++ build.py
@@ -13,24 +13,24 @@ if len(sys.argv) > 1:
         isDebug = True
 
 # Build the c++ protobuf
-buildExitCode = os.system("bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 --action_env PYTHON_BIN_PATH=\"%s\" avatar_message_proto_cc" % pythonBinPath)
+buildExitCode = os.system("bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 --action_env PYTHON_BIN_PATH=\"%s\" hallway_avatar_message_pb_cc" % pythonBinPath)
 if not buildExitCode == 0:
     print('Failed to build c++ protobuf')
     exit()
 
 # Copy mediapipe built pb.h and pb.cc files to build/ 
 cppPath = "cpp"
-shutil.copyfile('bazel-bin/avatar_message_proto_cc_pb/avatar_message.pb.h', f'{cppPath}/include/avatar_message.pb.h')
-shutil.copyfile('bazel-bin/avatar_message_proto_cc_pb/avatar_message.pb.cc', f'{cppPath}/src/avatar_message.pb.cc')
+shutil.copyfile('bazel-bin/hallway_avatar_message_pb_cc_pb/hallway_avatar_message.pb.h', f'{cppPath}/include/hallway_avatar_message.pb.h')
+shutil.copyfile('bazel-bin/hallway_avatar_message_pb_cc_pb/hallway_avatar_message.pb.cc', f'{cppPath}/src/hallway_avatar_message.pb.cc')
 
 # Build the c-sharp protobuf
-buildExitCode = os.system("bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 --action_env PYTHON_BIN_PATH=\"%s\" avatar_message_proto_cs" % pythonBinPath)
+buildExitCode = os.system("bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 --action_env PYTHON_BIN_PATH=\"%s\" hallway_avatar_message_pb_cs" % pythonBinPath)
 if not buildExitCode == 0:
     print('Failed to build c-sharp protobuf')
     exit()
 
 # Copy mediapipe built pb.h and pb.cc files to build/ 
 csPath = "cs"
-shutil.copyfile('bazel-bin/avatar_message_proto_cs/AvatarMessage.cs', f'{csPath}/AvatarMessage.cs')
+shutil.copyfile('bazel-bin/hallway_avatar_message_pb_cs/HallwayAvatarMessage.cs', f'{csPath}/HallwayAvatarMessage.cs')
 
 
--- cpp/include/AvatarMessageUtils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "avatar_message.pb.h"
-#include <vector>
-
-namespace AvatarMessageUtils { // This namespace is free standing util functions and shouldn't be instantiated
-    size_t BlendshapesToOscVec(const BlendShapes blendShapes, std::vector<float>& osc_floats_out, bool mirror = false);
-};
